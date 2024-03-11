a6ba59c925fcfefc81980ca3e85a6b7f5e5cb9dd
irina <irinafrutiz@gmail.com>
2024-03-11 17:50:27 +0100

    added: lesson 13 Maven, TestNG.

---
+++ src/test/java/SomethingTest.java
@@ -0,0 +1,9 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SomethingTest {
+    @Test
+    public void testSomething() {
+        Assert.assertEquals(0, 0);
+    }
+}
